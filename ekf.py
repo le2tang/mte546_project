9ee0cee210f0d43b40996629b2b5f5a42295c174
le2tang <le2tang@uwaterloo.ca>
2023-03-14 16:09:56 -0400

    Add comments to files

--- ekf.py
+++ ekf.py
@@ -9,12 +9,14 @@ class EKF:
         self.state_covar = np.eye(13)
 
     def predict(self):
+        """Update prior state estimate and covariance"""
         self.state = self.model.forward_model(self.state)
 
         # Update state covariance
         raise NotImplementedError()
 
     def correct(self, measurement):
+        """Update prior state estimate and covariance"""
         self.model_meas = self.model.measurement_model(self.state)
 
         # Update state covariance from kalman gain
--- model_equations.py
+++ model_equations.py
@@ -26,16 +26,21 @@ class PoseModel:
         raise NotImplementedError()
 
     def _state_position(self, state):
+        """Extract position from state vector"""
         return np.reshape(state[:3], (3, 1))
 
     def _state_lin_velocity(self, state):
+        """Extract linear velocity from state vector"""
         return np.reshape(state[3:6], (3, 1))
 
     def _state_orientation(self, state):
+        """Extract orientation from state vector in q = [x y z w] format"""
         return np.reshape(state[6:10], (4, 1))
 
     def _state_ang_velocity(self, state):
+        """Extract angular velocity from state vector"""
         return np.reshape(state[10:], (3, 1))
 
     def _vec2quat(self, vec3d):
-        return np.concatenate((vec3d, [[0]]))  # q = [x y z w] format
+        """Convert 3x1 vector to a quaternion in q = [x y z w] format"""
+        return np.concatenate((vec3d, [[0]]))
