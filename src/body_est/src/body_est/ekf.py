a6bcc630a6faba4e056c74d19597921daf45224e
Joanna Diao <joannadiao@hotmail.com>
2023-03-16 15:47:02 -0400

    implemented EKF methods

--- src/body_est/src/body_est/ekf.py
+++ src/body_est/src/body_est/ekf.py
@@ -7,17 +7,36 @@ class EKF:
 
         self.state = np.zeros((13, 1))  # [p, v, q, w]
         self.state_covar = np.eye(13)
+        self.process_noise = np.eye(13)
+        self.measurement_noise = np.eye(7) # does the number of states = number of measurements?
 
     def predict(self):
         """Update prior state estimate and covariance"""
+        A = self.model.forward_jacobian(self.state)
+        
         self.state = self.model.forward_model(self.state)
+        
+        self.state_covar = A * self.state_covar * A.T + self.process_noise
 
-        # Update state covariance
-        raise NotImplementedError()
+        return self.state, self.state_covar
 
     def correct(self, measurement):
         """Update prior state estimate and covariance"""
         self.model_meas = self.model.measurement_model(self.state)
+        
+        measurement_residual = measurement - self.model_meas
+        
+        # Calculate the measurement residual covariance
+        C = self.model.measurement_jacobian(self.state)
+        S_k = C * self.state_covar * C.T + self.measurement_noise
+        
+        # Calculate the Kalman gain
+        K_k = self.state_covar * C.T * np.linalg.pinv(S_k)
+        
+        # Update state estimate for time k
+        self.state = self.state + K_k * measurement_residual
+        
+        # Update state covariance estimate for time k
+        self.state_covar = self.state_covar - K_k * C * self.state_covar
 
-        # Update state covariance from kalman gain
-        raise NotImplementedError()
+        return self.state, self.state_covar
