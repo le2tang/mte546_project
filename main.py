74473f2409e34435602e63c0d9ac7adae7810d7f
Lyndon Tang <44735835+le2tang@users.noreply.github.com>
2023-03-29 16:40:34 -0400

    Change realsense to report body points

---
+++ main.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+import tf2_ros
+import tf_conversions
+
+from geometry_msgs.msg import PolygonStamped, Transform, TransformStamped
+from body_est.ekf import EKF
+from body_est.fit_anatomical_frame import BodyPolygon, FitAnatomicalFrame
+from body_est.model_equations import PoseModel
+import matplotlib.pyplot as plt
+
+
+class ViconInterface:
+    TF_REF_NAME = "vicon_world"
+    CAMERA_NAME = "camera"
+    BODY_NAME = "body"
+
+    def __init__(self):
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+
+    def get_cam_to_body(self):
+        try:
+            return self.tf_buffer.lookup_transform(
+                self.CAMERA_NAME, self.BODY_NAME, rospy.Time()
+            )
+        except (
+            tf2_ros.LookupException,
+            tf2_ros.ConnectivityException,
+            tf2_ros.ExtrapolationException,
+        ) as e:
+            rospy.logerr(
+                f"Transform lookup from {self.CAMERA_NAME} to {self.BODY_NAME} failed:\n{e}"
+            )
+            return None
+
+
+class EKFInterface:
+    def __init__(self):
+        self.ekf = EKF(PoseModel())
+
+    def get_position(self):
+        return self.ekf.state[:3].flatten()
+
+    def get_orientation(self):
+        # Return quaternion as [x y z w]
+        return (self.ekf.state[6:10] / np.sqrt(np.sum(np.square(self.ekf.state[6:10])))).flatten()
+
+    def get_k(self):
+        return self.ekf.K_k
+
+    def get_k_norm(self):
+        return np.linalg.norm(self.ekf.K_k)
+
+    def get_transform(self):
+        position = self.get_position()
+        orientation = self.get_orientation()
+
+        tf = Transform()
+        tf.translation.x = position[0]
+        tf.translation.y = position[1]
+        tf.translation.z = position[2]
+        tf.rotation.x = orientation[0]
+        tf.rotation.y = orientation[1]
+        tf.rotation.z = orientation[2]
+        tf.rotation.w = orientation[3]
+        return tf
+
+    def predict(self):
+        self.ekf.predict()
+
+    def correct(self, measurement):
+        self.ekf.correct(measurement)
+
+
+class BodyPoseNode:
+    def __init__(self):
+        rospy.init_node("body_pose_node")
+        
+        self.body_pts_sub = rospy.Subscriber("torso_polygon", PolygonStamped, self.check_body_points)
+
+        self.ekf = EKFInterface()
+        self.fit_anatomical_frame = FitAnatomicalFrame()
+
+        self.vicon = ViconInterface()
+        self.tf_broadcaster = tf2_ros.TransformBroadcaster()
+        # TODO fix thresholds or give better initial estimate since error
+        # is very large at beginning which prevents anything from running
+        self.dist_error_threshold = 10 
+        self.ang_error_threshold = 2 
+        self.dist_errs = []
+        self.ang_errs = []
+        
+        self.measured = []
+        self.filtered = []
+        self.k = []
+
+    def update(self, msg):
+        # Get the a priori estimate of the current state
+        self.ekf.predict()
+        predict_tf = self.ekf.get_transform()
+
+        body_pts = msg.points
+        body_tf = self.fit_anatomical_frame(
+            body_pts[BodyPolygon.LEFT_SHOULDER.value],
+            body_pts[BodyPolygon.RIGHT_SHOULDER.value],
+            body_pts[BodyPolygon.TORSO.value],
+        )
+
+        if self.validate_meas(body_pts, predict_tf, body_tf):
+            measurement = np.array(
+                [
+                    body_tf.translation.x,
+                    body_tf.translation.y,
+                    body_tf.translation.z,
+                    body_tf.rotation.x,
+                    body_tf.rotation.y,
+                    body_tf.rotation.z,
+                    body_tf.rotation.w,
+                ]
+            )
+            self.ekf.correct(measurement)
+            self.measured.append(measurement)
+
+        # Report the posterior estimate transform
+        estimate_tf = TransformStamped()
+        estimate_tf.header.stamp = rospy.Time.now()
+        estimate_tf.header.frame_id = "camera_link"
+        estimate_tf.child_frame_id = "body_est/body"
+        estimate_tf.transform = self.ekf.get_transform()
+        filtered = np.array(
+            [
+                estimate_tf.transform.translation.x,
+                estimate_tf.transform.translation.y,
+                estimate_tf.transform.translation.z,
+                estimate_tf.transform.rotation.x,
+                estimate_tf.transform.rotation.y,
+                estimate_tf.transform.rotation.z,
+                estimate_tf.transform.rotation.w,
+            ]
+        )
+        self.filtered.append(filtered)
+ 
+        #rospy.loginfo(f"body filtered {estimate_tf.transform}")
+
+        # Broadcast Transform from EKF prediction
+        self.tf_broadcaster.sendTransform(estimate_tf)
+
+        # ground_tf = self.vicon.get_cam_to_body()
+
+        # error_tf, (rot_axis, rot_angle) = self.get_error(
+        #    ground_tf, dist_error, estimate_tf.transform
+        # )
+        self.k.append(self.ekf.get_k_norm())
+
+    def validate_meas(self, body_pts, predict_tf, body_tf):
+        # Compare the measurement to the prior estimate
+        error_tf, dist_error, (rot_angle, rot_axis) = self.get_error(
+            predict_tf, body_tf
+        )
+        # Ignore the measurement if the difference is too large
+        rospy.loginfo(f"dist err {dist_error} ang err {rot_angle}")
+        if (dist_error < self.dist_error_threshold) and (
+            rot_angle.all() < self.ang_error_threshold
+        )
+
+    def get_error(self, ref_tf, link_tf):
+        error_tf = Transform()
+
+        error_tf.translation.x = link_tf.translation.x - ref_tf.translation.x
+        error_tf.translation.y = link_tf.translation.y - ref_tf.translation.y
+        error_tf.translation.z = link_tf.translation.z - ref_tf.translation.z
+
+        dist_error = np.sqrt(
+            np.sum(
+                np.square(
+                    [
+                        error_tf.translation.x,
+                        error_tf.translation.y,
+                        error_tf.translation.z,
+                    ]
+                )
+            )
+        )
+
+        ref_rot_mat = tf_conversions.transformations.quaternion_matrix(
+            [
+                ref_tf.rotation.x,
+                ref_tf.rotation.y,
+                ref_tf.rotation.z,
+                ref_tf.rotation.w,
+            ]
+        )
+        link_rot_mat = tf_conversions.transformations.quaternion_matrix(
+            [
+                link_tf.rotation.x,
+                link_tf.rotation.y,
+                link_tf.rotation.z,
+                link_tf.rotation.w,
+            ]
+        )
+
+        error_rot_mat = link_rot_mat @ ref_rot_mat.T
+        error_quat = tf_conversions.transformations.quaternion_from_matrix(
+            error_rot_mat
+        )
+        error_quat /= np.sqrt(np.sum(np.square(error_quat)))
+
+        error_tf.rotation.x = error_quat[0]
+        error_tf.rotation.y = error_quat[1]
+        error_tf.rotation.z = error_quat[2]
+        error_tf.rotation.w = error_quat[3]
+
+        rot_angle = 2 * np.arccos(error_quat[3])
+        rot_axis = error_quat[:3] / np.sqrt(1 - error_quat[3] * error_quat[3])
+
+        return error_tf, dist_error, (rot_angle, rot_axis)
+
+    def write_to_file(self):
+        plt.plot(self.k)
+        plt.xlabel("Iteration #")
+        plt.ylabel("K gain norm val")
+        plt.title("K gain norm trend")
+        plt.savefig("/home/felix/mte546/mte546_project/k_gain.png")
+
+        self.measured = np.array(self.measured)
+        self.filtered = np.array(self.filtered)
+        rospy.loginfo("x y z qz qy qz qw")
+        rospy.loginfo(f"measured var {np.var(self.measured, axis=0)}")
+        rospy.loginfo(f"filtered var {np.var(self.filtered, axis=0)}")
+        rospy.loginfo(f"measured max delta {np.max(self.measured, axis=0) - np.min(self.measured, axis=0)}")
+        rospy.loginfo(f"filtered max delta {np.max(self.filtered, axis=0) - np.min(self.filtered, axis=0)}")
+
+
+        self.measured_diff = np.diff(self.measured, axis=0)
+        self.filtered_diff = np.diff(self.filtered, axis=0)
+
+        # plot position trends
+        plt.clf()
+        plt.plot(self.measured[:, 0], label="Meas pX")
+        plt.plot(self.measured[:, 1], label="Meas pY")
+        plt.plot(self.measured[:, 2], label="Meas pZ")
+        plt.plot(self.filtered[:, 0], label="filtered pX")
+        plt.plot(self.filtered[:, 1], label="filtered pY")
+        plt.plot(self.filtered[:, 2], label="filtered pZ")
+        plt.legend()
+        plt.xlabel("Iteration #")
+        plt.ylabel("value (m)")
+        plt.title("Positions")
+        plt.savefig("/home/felix/mte546/mte546_project/positions.png")
+
+        # plot quaterion trends
+        plt.clf()
+        plt.plot(self.measured[:, 3], label="Meas qX")
+        plt.plot(self.measured[:, 4], label="Meas qY")
+        plt.plot(self.measured[:, 5], label="Meas qZ")
+        plt.plot(self.measured[:, 6], label="Meas qW")
+        plt.plot(self.filtered[:, 3], label="filtered qX")
+        plt.plot(self.filtered[:, 4], label="filtered qY")
+        plt.plot(self.filtered[:, 5], label="filtered qZ")
+        plt.plot(self.filtered[:, 6], label="filtered qW")
+        plt.legend()
+        plt.xlabel("Iteration #")
+        plt.ylabel("value (rad)")
+        plt.title("Quaterions")
+        plt.savefig("/home/felix/mte546/mte546_project/quat.png")
+
+        # plot position diff trends
+        plt.clf()
+        plt.plot(self.measured_diff[:, 0], label="Meas pX")
+        plt.plot(self.measured_diff[:, 1], label="Meas pY")
+        plt.plot(self.measured_diff[:, 2], label="Meas pZ")
+        plt.plot(self.filtered_diff[:, 0], label="filtered pX")
+        plt.plot(self.filtered_diff[:, 1], label="filtered pY")
+        plt.plot(self.filtered_diff[:, 2], label="filtered pZ")
+        plt.legend()
+        plt.xlabel("Iteration #")
+        plt.ylabel("value (m)")
+        plt.title("Position deltas")
+        plt.savefig("/home/felix/mte546/mte546_project/position_deltas.png")
+
+        # plot quaterion trends
+        plt.clf()
+        plt.plot(self.measured_diff[:, 3], label="Meas qX")
+        plt.plot(self.measured_diff[:, 4], label="Meas qY")
+        plt.plot(self.measured_diff[:, 5], label="Meas qZ")
+        plt.plot(self.measured_diff[:, 6], label="Meas qW")
+        plt.plot(self.filtered_diff[:, 3], label="filtered qX")
+        plt.plot(self.filtered_diff[:, 4], label="filtered qY")
+        plt.plot(self.filtered_diff[:, 5], label="filtered qZ")
+        plt.plot(self.filtered_diff[:, 6], label="filtered qW")
+        plt.legend()
+        plt.xlabel("Iteration #")
+        plt.ylabel("value (rad)")
+        plt.title("Quaterion deltas")
+        plt.savefig("/home/felix/mte546/mte546_project/quat_deltas.png")
+
+
+if __name__ == "__main__":
+    body_pose_node = BodyPoseNode()
+    rospy.spin()
+    rospy.on_shutdown(body_pose_node.write_to_file)
---
+++ realsense.py
@@ -0,0 +1,396 @@
+import rospy
+from actionlib import SimpleActionServer
+
+from tf2_geometry_msgs import PointStamped
+import tf2_msgs.msg
+from geometry_msgs.msg import TransformStamped, PolygonStamped, Polygon
+from tf.transformations import quaternion_from_euler, quaternion_from_matrix
+import tf2_ros
+from std_msgs.msg import Int32
+
+from sensor_msgs.msg import Image, CameraInfo
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+import mediapipe as mp
+import pyrealsense2 as rs
+import numpy as np
+import time
+import math
+import copy
+from enum import Enum
+
+
+class Landmarks(Enum):
+    # assign the integers to the useful ones
+    NOSE = 0
+    LEFT_EYE = 2
+    RIGHT_EYE = 5
+    LEFT_EAR = 7
+    RIGHT_EAR = 8
+    MOUTH_LEFT = 9
+    MOUTH_RIGHT = 10
+    LEFT_SHOULDER = 11
+    RIGHT_SHOULDER = 12
+    LEFT_WRIST = 15
+    RIGHT_WRIST = 16
+    LEFT_THUMB = 21
+    RIGHT_THUMB = 22
+    LEFT_HIP = 23
+    RIGHT_HIP = 24
+
+
+class PoseEstimation:
+    def __init__(self):
+        self.logger_name = "pose_estimation"
+        rospy.init_node("body_est", anonymous=True)
+        rospy.loginfo(
+            "Starting the Perception pose estimation node",
+            logger_name=self.logger_name,
+        )
+        self.got_new_depth = False
+        self.got_intrinsics = False
+
+        # Rviz visualization and published topic
+        self.point_viz_topic = "/point_viz"
+        self.point_viz_pub = rospy.Publisher(
+            self.point_viz_topic, PointStamped, queue_size=10
+        )
+
+        # transforms
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        self.ref_link = "camera_link"
+        self.depth_optical_link_frame = "optical_depth_link_frame"
+
+        # landmarks
+        self.lm_body = [
+            Landmarks.RIGHT_SHOULDER,
+            Landmarks.LEFT_SHOULDER,
+            Landmarks.RIGHT_HIP,
+            Landmarks.LEFT_HIP,
+        ]
+
+        # TODO need to set a threshold to determine if joint is visible
+        self.landmark_vis_thresh = 0.9
+
+        # OpenCv
+        self.cv_bridge = CvBridge()
+        self.depth_img = None
+        self.rgb_img = None
+        self.camera_intrinsics = rs.intrinsics()
+        self.MM_TO_M = 1 / 1000
+        time.sleep(1.5)
+
+        # camera data subs
+        self.get_camera_info()
+        self.depth_sub = rospy.Subscriber(
+            "camera/aligned_depth_to_color/image_raw", Image, self.depth_image_cb
+        )
+        self.image_sub = rospy.Subscriber(
+            "camera/color/image_raw", Image, self.rgb_image_cb
+        )
+
+        # Rotation matrices
+        self.x_unit = np.array([1, 0, 0])
+        self.y_unit = np.array([0, 1, 0])
+        self.z_unit = np.array([0, 0, 1])
+
+        # transform pub
+        self.torso_tf_pub = rospy.Publisher("/tf", tf2_msgs.msg.TFMessage, queue_size=1)
+        self.torso_only_tf_pub = rospy.Publisher(
+            "torso_tf", TransformStamped, queue_size=1
+        )
+
+        # polygon pub
+        self.torso_polygon_pub = rospy.Publisher(
+            "/torso_polygon", PolygonStamped, queue_size=1
+        )
+
+        # new iteration pub
+        self.pose_updated_pub = rospy.Publisher("/pose_updated", Int32, queue_size=1)
+        self.updates_counter = 0
+
+    def depth_image_cb(self, data):
+        # store the latest depth image for processing
+        try:
+            self.depth_img = self.cv_bridge.imgmsg_to_cv2(data, "16UC1")
+            self.got_new_depth = True
+        except CvBridgeError as e:
+            rospy.loginfo(e)
+
+    def rgb_image_cb(self, data):
+        # store the latest color image for processing
+        try:
+            self.rgb_img = self.cv_bridge.imgmsg_to_cv2(data, "bgr8")
+            # only execute when have updated instrinsics, depth, and rgb
+            if self.got_new_depth == True and self.got_intrinsics == True:
+                # set updated depth to be false to wait until new depth and rgb
+                self.got_new_depth = False
+                with mp.solutions.pose.Pose(
+                    static_image_mode=True,
+                    model_complexity=2,
+                    enable_segmentation=True,
+                    min_detection_confidence=0.5,
+                ) as pose:
+                    self.rgb_img.flags.writeable = False
+                    image = cv2.cvtColor(self.rgb_img, cv2.COLOR_BGR2RGB)
+                    results = pose.process(image)
+                    landmarks_list = results.pose_landmarks.landmark
+                    
+                    torso = self.pose_estimate_body(landmarks_list)
+                    rospy.loginfo(f"torso {torso}")
+
+                    t = TransformStamped()
+                    t.header.frame_id = "/camera_link"
+                    t.header.stamp = rospy.Time.now()
+                    t.child_frame_id = "torso"
+                    t.transform.translation.x = torso[0]
+                    t.transform.translation.y = torso[1]
+                    t.transform.translation.z = torso[2]
+                    t.transform.rotation.x = torso[3]
+                    t.transform.rotation.y = torso[4]
+                    t.transform.rotation.z = torso[5]
+                    t.transform.rotation.w = torso[6]
+                    tfm = tf2_msgs.msg.TFMessage([t])
+                    self.torso_tf_pub.publish(tfm)
+                    self.torso_only_tf_pub.publish(t)
+
+                    # update topic to signal that count has incremented
+                    self.updates_counter = self.updates_counter + 1
+                    counter_msg = Int32()
+                    counter_msg.data = self.updates_counter
+                    self.pose_updated_pub.publish(self.updates_counter)
+
+        except CvBridgeError as e:
+            rospy.loginfo(e)
+
+    def get_camera_info(self):
+        info = rospy.wait_for_message(
+            "camera/aligned_depth_to_color/camera_info", CameraInfo
+        )
+
+        # plumb model is also known as brown conrady
+        self.depth_optical_link_frame = info.header.frame_id
+        if info.distortion_model == "plumb_bob":
+            self.camera_intrinsics.model = rs.distortion.brown_conrady
+        else:
+            self.camera_intrinsics.model = rs.distortion.none
+        # intrinsics given as 1D array
+        self.camera_intrinsics.fx = info.K[0]
+        self.camera_intrinsics.fy = info.K[4]
+        self.camera_intrinsics.ppx = info.K[2]
+        self.camera_intrinsics.ppy = info.K[5]
+        self.camera_intrinsics.width = info.width
+        self.camera_intrinsics.height = info.height
+        rospy.loginfo(f"Got camera intrinsics {self.camera_intrinsics}")
+        self.got_intrinsics = True
+
+    def pose_estimate_body(self, landmarks_list):
+        # dict {'landmark_id': {'mp_landmark': landmark, 'point': PointStamped}}
+        landmarks = {}
+        # get important landmarks
+        for lm in self.lm_body:
+            # add empty pose stamped since landmark position is not true
+            landmark_data = landmarks_list[lm.value]
+
+            # if one of the points are not visible then fail
+            if landmark_data.visibility < self.landmark_vis_thresh:
+                return
+
+            lm_point = PointStamped()
+            lm_point.header.frame_id = self.depth_optical_link_frame
+            lm_point.point.x = landmark_data.x
+            lm_point.point.y = landmark_data.y
+            lm_point.point.z = landmark_data.z
+            landmarks[lm.value] = {
+                "mp_landmark": landmark_data,
+                "point": lm_point,
+            }
+        # rospy.loginfo(f"{landmarks}")
+
+        # get landmarks coords
+        for lm in self.lm_body:
+            # transform landmark to 3D pose and transform coords to base link
+            landmarks[lm.value]["point"] = self.transform_point_cameralink(
+                self.landmark_to_3d(landmarks[lm.value]["point"])
+            )
+            self.point_viz_pub.publish(landmarks[lm.value]["point"])
+            
+        torso_pt_stamped, torso_polygon = self.get_torso_pts(landmarks)
+        self.point_viz_pub.publish(torso_pt_stamped)
+
+        # returns a dictionary of the landmarks, their info, and their 3D coordinate
+        unit_nrml_x = self.compute_x_normal(landmarks, torso_pt_stamped.point)
+        # use shoulder vector as y unit normal
+        unit_nrml_y = self.compute_y_normal(landmarks) 
+        unit_nrml_z = np.cross(unit_nrml_x, unit_nrml_y)
+
+        R = np.stack((unit_nrml_x, unit_nrml_y, unit_nrml_z)).T
+        T = np.eye(4); T[:3, :3] = R
+        q = quaternion_from_matrix(T)
+
+        # rospy.loginfo(f"x {unit_nrml_x} y {unit_nrml_y} z {unit_nrml_z}")
+        # rospy.loginfo(f"xy_dot {np.dot(unit_nrml_x,unit_nrml_y)}")
+        # rospy.loginfo(f"xz_dot {np.dot(unit_nrml_x,unit_nrml_z)}")
+        # rospy.loginfo(f"yz_dot {np.dot(unit_nrml_y,unit_nrml_z)}")
+
+        #rospy.loginfo(f"{q}")
+        torso_pose = [
+            torso_point.x,
+            torso_point.y,
+            torso_point.z,
+            q[0],
+            q[1],
+            q[2],
+            q[3],
+        ]
+
+        return torso_pose
+
+    def compute_euler_ang(self, target_v, unit_v):
+        c = np.dot(target_v, unit_v) / np.linalg.norm(target_v) / np.linalg.norm(unit_v)
+        angle = np.arccos(c)
+        return angle
+
+    def compute_x_normal(self, landmarks, torso_point):
+        # takes in landmarks and finds unit normal of plane
+        # second point is the center point for the vectors
+        # to be "crossed" upon
+        p_rhip = landmarks[Landmarks.RIGHT_HIP.value]["point"].point
+        p_lhip = landmarks[Landmarks.LEFT_HIP.value]["point"].point
+        p_rsh = landmarks[Landmarks.RIGHT_SHOULDER.value]["point"].point
+        p_lsh = landmarks[Landmarks.LEFT_SHOULDER.value]["point"].point
+
+        vector_1 = np.array(
+            [
+                p_lsh.x - torso_point.x,
+                p_lsh.y - torso_point.y,
+                p_lsh.z - torso_point.z,
+            ]
+        )
+        # using another landmark
+        vector_2 = np.array(
+            [
+                p_rsh.x - torso_point.x,
+                p_rsh.y - torso_point.y,
+                p_rsh.z - torso_point.z,
+            ]
+        )
+
+        # might need to check direction is no into bed
+        plane_normal = np.cross(vector_1, vector_2)
+        plane_unit_normal = plane_normal / np.linalg.norm(plane_normal)
+        return plane_unit_normal
+
+    def compute_y_normal(self, landmarks):
+        p_rsh = landmarks[Landmarks.RIGHT_SHOULDER.value]["point"].point
+        p_lsh = landmarks[Landmarks.LEFT_SHOULDER.value]["point"].point
+
+        vector_1 = np.array(
+            [
+                p_lsh.x - p_rsh.x,
+                p_lsh.y - p_rsh.y,
+                p_lsh.z - p_rsh.z,
+            ]
+        )
+
+        # might need to check direction is no into bed
+        y_unit_normal = vector_1 / np.linalg.norm(vector_1)
+        return y_unit_normal
+
+    def get_torso_pts(self, landmarks):
+        left_shoulder = landmarks[Landmarks.LEFT_SHOULDER.value]["point"].point
+        right_shoulder = landmarks[Landmarks.RIGHT_SHOULDER.value]["point"].point
+
+        left_hip = landmarks[Landmarks.LEFT_HIP.value]["point"].point
+        right_hip = landmarks[Landmarks.RIGHT_HIP.value]["point"].point
+
+        mid_shoulder = Point()
+        mid_shoulder.x = 0.5 * (left_shoulder.x + right_shoulder.x)
+        mid_shoulder.y = 0.5 * (left_shoulder.y + right_shoulder.y)
+        mid_shoulder.z = 0.5 * (left_shoulder.z + right_shoulder.z)
+
+        mid_hip = Point()
+        mid_hip.x = 0.5 * (left_hip.x + right_hip.x)
+        mid_hip.y = 0.5 * (left_hip.y + right_hip.y)
+        mid_hip.z = 0.5 * (left_hip.z + right_hip.z)
+
+        medial_lateral = np.array([[right_shoulder.x - left_shoulder.x], [right_shoulder.y - left_shoulder.y], [right_shoulder.z - left_shoulder.z]])
+        antero_posterior np.array([[mid_hip.x - mid_shoulder.x], [mid_hip.y - mid_shoulder.y], [mid_hip.z - mid_shoulder.z]])
+        mid_shoulder_pvec = np.array([[mid_shoulder.x], [mid_shoulder.y], [mid_shoulder.z]])
+
+        chest_pvec = mid_shoulder_pvec + 0.25 * (antero_posterior.T @ medial_lateral) / (medial_lateral.T @ medial_lateral) * medial_lateral
+        torso_point = Point(x=chest_pvec[0], y=chest_pvec[1], z=chest_pvec[2])
+
+        torso_polygon = PolygonStamped()
+        torso_polygon.header.frame_id = "/camera_link"
+        torso_polygon.header.stamp = rospy.Time.now()
+        torso_polygon.polygon.points = [
+            left_shoulder,
+            mid_shoulder,
+            right_shoulder,
+            torso_point,
+            right_hip,
+            mid_hip,
+            left_hip,
+        ]
+        self.torso_polygon_pub.publish(torso_polygon)
+
+        torso_point_stamped = PointStamped()
+        torso_point_stamped.header.frame_id = landmarks[Landmarks.LEFT_SHOULDER.value][
+            "point"
+        ].header.frame_id
+        torso_point_stamped.point = torso_point
+
+        return torso_point_stamped, torso_polygon
+
+    def landmark_to_3d(self, point_stamped):
+        # Compute the 3D coordinate of each pose. 3D values in mm
+        x_pixel = int(point_stamped.point.x * self.camera_intrinsics.width)
+        y_pixel = int(point_stamped.point.y * self.camera_intrinsics.height)
+
+        depth = self.depth_img[y_pixel][x_pixel]
+
+        # librealsense 2 function see opencv_pointcloud_viewer.py
+        # inverse projection
+        point_vals = rs.rs2_deproject_pixel_to_point(
+            self.camera_intrinsics, [x_pixel, y_pixel], depth
+        )
+
+        # deproject returns an array of points
+        new_point_stamped = PointStamped()
+        new_point_stamped.header.frame_id = point_stamped.header.frame_id
+        new_point_stamped.point.x = point_vals[0] * self.MM_TO_M
+        new_point_stamped.point.y = point_vals[1] * self.MM_TO_M
+        new_point_stamped.point.z = point_vals[2] * self.MM_TO_M
+
+        return new_point_stamped
+
+    def transform_point_cameralink(self, point_stamped):
+        # transforms from the optical lens to the camera link
+        # flips point into more reasonable coordinate frame
+        while not rospy.is_shutdown():
+            try:
+                # rospy.loginfo(f"point before {point_stamped}")
+                transformed_point = self.tf_buffer.transform(
+                    point_stamped,
+                    self.ref_link,  # rospy.Time()
+                )
+                # rospy.loginfo(f"point after {transformed_point}")
+                return transformed_point
+            except (
+                tf2_ros.LookupException,
+                tf2_ros.ConnectivityException,
+                tf2_ros.ExtrapolationException,
+            ):
+                continue
+            rospy.sleep(0.2)
+
+
+def main():
+    server = PoseEstimation()
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    main()
